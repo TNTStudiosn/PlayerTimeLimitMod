ee4608a7f52e7cea691931192158f0a11682abf5
TNTStudios <tntstudiosn@gmail.com>
2025-04-03 08:26:39 -0600

    Initial commit

---
+++ src/main/java/com/TNTStudios/playertimelimit/Playertimelimit.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.playertimelimit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Playertimelimit implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
